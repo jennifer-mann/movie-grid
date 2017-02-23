c96ee30d3a7749ec3f4bc4e2ab292818366294a4
Jennifer Mann <jennifermann17@gmail.com>
2017-02-22 20:42:35 -0500

    style mock images.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,45 @@ import { Link } from 'react-router';
 export default class Main extends React.Component {
   render() {
     return (
-      <div className="app-container">
-        lolol
+      <div>
+        <ul className='movie-posters'>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+          <li>
+            <img src="https://image.tmdb.org/t/p/w500/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg" />
+          </li>
+        </ul>
       </div>
     );
   }
