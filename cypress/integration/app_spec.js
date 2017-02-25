affc3f60bbc535a7e15387be2ecee67b9b84f4fa
Jennifer Mann <jennifermann17@gmail.com>
2017-02-25 17:44:37 -0500

    Added tests for infinite scrolling.

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,5 +1,4 @@
 describe('App', function(){
-
   beforeEach(function(){
     cy
       .server()
@@ -42,6 +41,44 @@ describe('App', function(){
           .find("img")
             .should("have.attr", "src")
               .and("include", this.movies.results[0].poster_path)
+    })
+  })
+
+  context('infinite scrolling', function(){
+    beforeEach(function(){
+      cy
+        .route(/movie/, "fx:movies").as("getMovies")
+        .visit('/')
+        .wait("@getMovies")
+
+    })
+
+    it('loads more on scroll to bottom', function() {
+      cy
+        .route(/page=2/, "fx:movies").as("getMoviesPage2")
+        .get(".movie-posters > li").last().then(function(el){
+          (el)[0].scrollIntoView()
+        })
+        .wait("@getMoviesPage2")
+    })
+
+    it('does not load more if last page', function() {
+      this.movies.total_pages = 2
+
+      cy
+        .route(/page=2/, this.movies).as("getMoviesPage2")
+        .route(/page=3/, this.movies).as("getMoviesPage3")
+
+        .get(".movie-posters > li").last().then(function(el){
+          (el)[0].scrollIntoView()
+        })
+        .wait("@getMoviesPage2")
+
+        .get(".movie-posters > li").last().then(function(el){
+          (el)[0].scrollIntoView()
+        })
+
+        .get("@getMoviesPage3.all").should("have.length", 0)
     })
   })
 })
